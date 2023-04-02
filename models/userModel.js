acddc8e7206f6ffe339ded1486d7d46f6fbd6f13
adorabletank <95323337+adorabletank@users.noreply.github.com>
2023-04-02 03:28:41 -0400

    initial commit

---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+// Import dependencies
+const mongoose = require('mongoose');
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+ 
+});
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+  
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) {
+      return next();
+    }
+  
+    // generate a salt
+    const salt = await bcrypt.genSalt(10);
+  
+    // hash the password using our new salt
+    const hash = await bcrypt.hash(user.password, salt);
+  
+    // override the cleartext password with the hashed one
+    user.password = hash;
+  
+    next();
+  });
+  
+
+// Define user model
+const userModel = mongoose.model('users', userSchema);
+
+// Export user model
+module.exports = userModel;
