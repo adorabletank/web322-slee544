85f5d9c64fdd07d83a179ed01d0838ac403bfc4e
adorabletank <95323337+adorabletank@users.noreply.github.com>
2023-04-02 23:53:23 -0400

    initial commit

--- controller/generalController.js
+++ controller/generalController.js
@@ -1,23 +1,43 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const path = require("path");
+const router = express.Router();
+const userModel = require(path.join(__dirname,'../models/userModel'));
 
-// router.get('/', (req, res) => {
-//     var featured_rentals = rentals.getFeaturedRentals();
-//     res.render('home', {
-//         data: featured_rentals
-//     });
-// });
+router.get('/', (req, res) => {
+    var featured_rentals = rentals.getFeaturedRentals();
+    res.render('home', {
+        data: featured_rentals
+    });
+});
 
-// router.get('/sign-up', (req, res) => {
-//     res.render('sign-up');
-// });
+router.post('/sign-up', async (req, res) => {
+    const { firstName, lastName, username, password, role } = req.body;
+  
+    try {
+      const user = new userModel({
+        firstName,
+        lastName,
+        username,
+        password,
+        role
+      });
+  
+      await user.save();
+  
+      res.redirect('/welcome');
+    } catch (error) {
+      console.error(error);
+      res.render('sign-up', { errorMessage: 'An error occurred while creating your account.' });
+    }
+  });
 
-// router.get('/log-in', (req, res) => {
-//     res.render('log-in');
-// });
+router.get('/log-in', (req, res) => {
+    res.render('log-in');
+});
 
-// router.get('/welcome', (req, res) => {
-//     res.render('welcome');
-// });
+router.get('/welcome', (req, res) => {
+    res.render('welcome');
+});
 
-// module.exports = router;
+module.exports = router;
+
--- models/userModel.js
+++ models/userModel.js
@@ -1,51 +1,58 @@
-// // Import dependencies
-// const mongoose = require('mongoose');
+// Import dependencies
+const mongoose = require('mongoose');
 
-// // Define user schema
-// const userSchema = new mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     required: true
-//   },
-//   lastName: {
-//     type: String,
-//     required: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   }
+// Define user schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+},
+role: {
+  type: String,
+  enum: ["data entry clerk", "customer"],
+  required: true,
+  default: "customer"
+}
  
-// });
+});
 
-// userSchema.pre("save", async function (next) {
-//     const user = this;
+userSchema.pre("save", async function (next) {
+    const user = this;
   
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified("password")) {
-//       return next();
-//     }
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) {
+      return next();
+    }
   
-//     // generate a salt
-//     const salt = await bcrypt.genSalt(10);
+    // generate a salt
+    const salt = await bcrypt.genSalt(10);
   
-//     // hash the password using our new salt
-//     const hash = await bcrypt.hash(user.password, salt);
+    // hash the password using our new salt
+    const hash = await bcrypt.hash(user.password, salt);
   
-//     // override the cleartext password with the hashed one
-//     user.password = hash;
+    // override the cleartext password with the hashed one
+    user.password = hash;
   
-//     next();
-//   });
+    next();
+  });
   
 
-// // Define user model
-// const userModel = mongoose.model('users', userSchema);
+// Define user model
+const userModel = mongoose.model('users', userSchema);
 
-// // Export user model
-// module.exports = userModel;
+// Export user model
+module.exports = userModel;
+
